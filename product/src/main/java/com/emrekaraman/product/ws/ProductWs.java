b7fd2170c2c0ef6cf15658a7917ffb04a2bda740
karamanemre <7emre10@gmail.com>
2022-07-07 14:18:27 +0300

    revize (spring.cloud.compatibility-verifier.enabled=false)

--- product/src/main/java/com/emrekaraman/product/ws/ProductWs.java
+++ product/src/main/java/com/emrekaraman/product/ws/ProductWs.java
@@ -1,6 +1,12 @@
 package com.emrekaraman.product.ws;
 
-public class ProductWs {
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/productws")
+public class ProductWs {
 
 }
